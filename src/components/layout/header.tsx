cb3e3960523110a769357569524b9c9816b5366b
earnieacts <earnieactub@buksu.edu.ph>
2024-11-23 21:25:56 +0800

    style: modified header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import WindowControls from './window-controls';
+// import WindowControls from './window-controls';
 import ThemeToggle from './theme-toggle';
 import { useTheme } from '@/hooks/use-theme';
 
 const Header: React.FC = () => {
-  const isMac = window.navigator.platform.toLowerCase().includes('mac');
+
   const { theme, setTheme } = useTheme();
 
   return (
     <header 
-      className="bg-gray-100 dark:bg-black p-2 flex justify-between items-center select-none" 
+      className="bg-gray-100 dark:bg-black p-2 flex justify-end items-center select-none" 
       style={{ WebkitAppRegion: 'drag' } as React.CSSProperties}
     >
-      {isMac && <WindowControls />}
-      <div className="text-gray-900 dark:text-white text-sm">EVENT TIMER</div>
       <div 
         className="flex items-center gap-4" 
         style={{ WebkitAppRegion: 'no-drag' } as React.CSSProperties}
       >
         <ThemeToggle currentTheme={theme} onThemeChange={setTheme} />
-        {!isMac && <WindowControls />}
+      
       </div>
     </header>
   );
